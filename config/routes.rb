e28edd4330528451f1c59756949ebbc838baf49f
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-11-28 16:43:01 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :users
+    resources :events
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
