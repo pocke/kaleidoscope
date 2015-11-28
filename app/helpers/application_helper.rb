c57530f6edf228c902718c145a016d256a1b6330
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-11-28 21:02:30 +0900

    Alert

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def bootstrap_flash(options = {})
+    flash_messages = []
+    flash.each do |type, message|
+      # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
+      next if message.blank?
+
+      type = type.to_sym
+      type = :success if type.to_s == :notice.to_s
+      type = :danger if type.to_s == :alert.to_s
+
+      Array(message).each do |msg|
+        text = content_tag(:div,
+          content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") + msg,
+          class: "alert fade in alert-#{type} alert-dismissible #{options[:class]}",
+        )
+        flash_messages << text if msg
+      end
+    end
+    flash_messages.join("\n").html_safe
+  end
 end
