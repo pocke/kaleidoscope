8803bf0c5fa85a94d652ae9257159e63c9a7178a
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-11-28 20:07:55 +0900

    Controller and rotue

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  Rails.application.routes.draw do
+    devise_for :users, controllers: {
+      sessions: 'users/sessions'
+    }
+  end
+
   root 'root#index'
 
   namespace :api do
