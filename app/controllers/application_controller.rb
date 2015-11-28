c56b3bd5b79dab8ed05794e35b43826193936e4f
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-11-28 19:24:21 +0900

    Bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :cors
   protect_from_forgery with: :null_session
+
   def cors
     headers['Access-Control-Allow-Origin'] = '*'
   end
