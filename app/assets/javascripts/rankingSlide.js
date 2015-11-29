41f0ab5433d6061fb429f000328cbc49a96f2797
Tatsuya Oba <toba17893@gmail.com>
2015-11-29 11:06:05 +0900

    ランキングページ（途中）

---
+++ app/assets/javascripts/rankingSlide.js
@@ -0,0 +1,24 @@
+$('#rank1-content').slideToggle();
+$('#rank1').click(function () {
+    $('#rank1-content').slideToggle();
+});
+
+$('#rank2-content').slideToggle();
+$('#rank2').click(function () {
+    $('#rank2-content').slideToggle();
+});
+
+$('#rank3-content').slideToggle();
+$('#rank3').click(function () {
+    $('#rank3-content').slideToggle();
+});
+
+$('#rank4-content').slideToggle();
+$('#rank4').click(function () {
+    $('#rank4-content').slideToggle();
+});
+
+$('#rank5-content').slideToggle();
+$('#rank5').click(function () {
+    $('#rank5-content').slideToggle();
+});
