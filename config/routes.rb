5713e2c1e776c8cc679b26bd5d6c5f84d5603fec
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-11-29 10:02:09 +0900

    Font

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,8 @@ Rails.application.routes.draw do
     end
   end
 
+  # for font
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
