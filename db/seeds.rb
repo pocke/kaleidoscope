b47bdab60a5e6591be4920e8b6c15460377e003c
Fujiyama Shingo <gyama128@gmail.com>
2015-12-01 14:59:41 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ c = Company.find_or_create_by!(name: "SLOGAN")
 e = Event.find_or_create_by!(
   name: "Hacker Wars II", 
   start_date: Time.utc(2015, 11, 28, 10), 
-  end_date: Time.utc(2015, 11, 29, 16),
+  end_date: Time.utc(2015, 12, 5, 16),
   company: c,
   prize: "10万円",
   description: "日本一の学生エンジニアを決定するハッカソン･イベント",
